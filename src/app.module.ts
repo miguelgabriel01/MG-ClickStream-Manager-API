8028155ba09f4ce87ad476d20235123ffc8139fd
miguelgabriel01 <gabrielogabriel10@gmail.com>
2024-08-01 11:25:45 -0300

    feature: criação de configuração de banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
+// app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    UsersModule,
+  ],
 })
 export class AppModule {}
+
+/*ConfigModule.forRoot(): Carrega as variáveis de ambiente do arquivo .env.
+MongooseModule.forRoot(): Conecta ao MongoDB usando a URL de conexão fornecida nas variáveis de ambiente.*/
