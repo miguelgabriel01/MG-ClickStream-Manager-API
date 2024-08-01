ea1db9e665db7c7bef9f65674ecc9038d25fef8d
miguelgabriel01 <gabrielogabriel10@gmail.com>
2024-08-01 13:26:42 -0300

    feature: criação de serviço(service) de autenticação

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+// auth/auth.service.ts
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
+import { UsersService } from '../users/users.service';
+import { LoginDto } from './dto/login.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.usersService.findByEmail(email);
+    if (user && await bcrypt.compare(password, user.password)) {
+      return { id: user.id, email: user.email };
+    }
+    throw new UnauthorizedException('Invalid email or password');
+  }
+
+  async login(loginDto: LoginDto) {
+    const user = await this.validateUser(loginDto.email, loginDto.password);
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
