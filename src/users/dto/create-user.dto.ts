25cb55adb9a34de17fd890750f1e73ae6cb19432
miguelgabriel01 <gabrielogabriel10@gmail.com>
2024-08-01 11:36:34 -0300

    feature: criação de dto para validação de dados do usuário

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,20 @@
+// users/dto/create-user.dto.ts
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
