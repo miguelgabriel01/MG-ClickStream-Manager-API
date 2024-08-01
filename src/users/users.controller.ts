46baa5fab95a503897623b41c9a630f0bc5f3117
miguelgabriel01 <gabrielogabriel10@gmail.com>
2024-08-01 11:43:59 -0300

    feature: implementação do controlador(controller) do usuário

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+// users/users.controller.ts
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('v1/users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('create')
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createUserDto: CreateUserDto) {
+    await this.usersService.create(createUserDto);
+    return { message: 'User registered successfully' };
+  }
+}
