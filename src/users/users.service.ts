8712f6e127a186f2c46f0aa07d63b67266d0bd97
miguelgabriel01 <gabrielogabriel10@gmail.com>
2024-08-01 11:40:47 -0300

    feature: implementação de serviço(service) do usuario

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+// users/users.service.ts
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
+import { User } from './schemas/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<void> {
+    // Check if the user already exists
+    const existingUser = await this.userModel.findOne({ email: createUserDto.email });
+    if (existingUser) {
+      throw new BadRequestException('Email already in use');
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+
+    // Create a new user
+    const newUser = new this.userModel({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+
+    // Save the user to the database
+    await newUser.save();
+  }
+}
