cf30f045efc8b4660654de038e397542fb31db60
miguelgabriel01 <gabrielogabriel10@gmail.com>
2024-08-01 13:30:29 -0300

    feature: criação de configurações do jwt

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+// auth/auth.module.ts
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'secretKey', // Substitua por uma chave secreta mais segura em produção
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
+})
+export class AuthModule {}
