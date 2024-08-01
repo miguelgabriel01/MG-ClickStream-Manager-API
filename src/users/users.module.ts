fd90fd46eeee1422d3a930378bad5479eff64731
miguelgabriel01 <gabrielogabriel10@gmail.com>
2024-08-01 11:45:28 -0300

    feature: implementação do mongoose e schemas ao modulo de usuário

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,15 @@
+// users/users.module.ts
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { User, UserSchema } from './schemas/user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
